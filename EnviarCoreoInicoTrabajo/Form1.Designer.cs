5f1b4c4bc31494dcbcf82c86a968107babacd4dd
Gregorio Oduber <Gregorio Oduber@STSA-E1-1CN6P73>
2023-04-06 10:15:04 -0500

    Agregar archivos de proyecto.

---
+++ EnviarCoreoInicoTrabajo/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace EnviarCoreoInicoTrabajo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.enviarCorreoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.TSTBHora = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.salirToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.activarTemporizadorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 30000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.enviarCorreoToolStripMenuItem,
+            this.TSTBHora,
+            this.toolStripMenuItem1,
+            this.activarTemporizadorToolStripMenuItem,
+            this.salirToolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(187, 123);
+            // 
+            // enviarCorreoToolStripMenuItem
+            // 
+            this.enviarCorreoToolStripMenuItem.Name = "enviarCorreoToolStripMenuItem";
+            this.enviarCorreoToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.enviarCorreoToolStripMenuItem.Text = "Enviar Correo";
+            this.enviarCorreoToolStripMenuItem.Click += new System.EventHandler(this.enviarCorreoToolStripMenuItem_Click);
+            // 
+            // TSTBHora
+            // 
+            this.TSTBHora.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TSTBHora.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.TSTBHora.Name = "TSTBHora";
+            this.TSTBHora.Size = new System.Drawing.Size(100, 23);
+            this.TSTBHora.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TSTBHora_KeyPress);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(157, 6);
+            // 
+            // salirToolStripMenuItem1
+            // 
+            this.salirToolStripMenuItem1.Image = ((System.Drawing.Image)(resources.GetObject("salirToolStripMenuItem1.Image")));
+            this.salirToolStripMenuItem1.Name = "salirToolStripMenuItem1";
+            this.salirToolStripMenuItem1.Size = new System.Drawing.Size(186, 22);
+            this.salirToolStripMenuItem1.Text = "Salir";
+            this.salirToolStripMenuItem1.Click += new System.EventHandler(this.salirToolStripMenuItem1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(180, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(180, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 64);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(180, 20);
+            this.textBox3.TabIndex = 1;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 3600000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // activarTemporizadorToolStripMenuItem
+            // 
+            this.activarTemporizadorToolStripMenuItem.Enabled = false;
+            this.activarTemporizadorToolStripMenuItem.Name = "activarTemporizadorToolStripMenuItem";
+            this.activarTemporizadorToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.activarTemporizadorToolStripMenuItem.Text = "Activar Temporizador";
+            this.activarTemporizadorToolStripMenuItem.Click += new System.EventHandler(this.activarTemporizadorToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 237);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Correo";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.contextMenuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem enviarCorreoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripTextBox TSTBHora;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.ToolStripMenuItem activarTemporizadorToolStripMenuItem;
+    }
+}
+
---
+++ EnviarCoreoInicoTrabajo/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using EnviarCoreoInicoTrabajo.Properties;
+
+namespace EnviarCoreoInicoTrabajo
+{
+    public partial class Form1 : Form
+    {
+        DateTime fechaActiva;
+        public Form1()
+        {
+            InitializeComponent();
+            TSTBHora.Text = Settings.Default.hora.ToString();
+            fechaActiva = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 18, 00, 0);
+        }
+
+        public void SendEmailWithOutlook(string mailDirection, string mailCopia, string mailSubject, string mainContent)
+        {
+            try
+            {
+                var oApp = new Microsoft.Office.Interop.Outlook.Application();
+
+                Outlook.NameSpace ns = oApp.GetNamespace("MAPI");
+                var f = ns.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderInbox);
+
+                System.Threading.Thread.Sleep(1000);
+
+                Outlook.Application outlookApp = new Outlook.Application();
+                Outlook.MailItem mailItem = (Outlook.MailItem)outlookApp.CreateItem(Outlook.OlItemType.olMailItem);
+
+                mailItem.Subject = mailSubject;
+                mailItem.HTMLBody = mainContent;
+                mailItem.To = mailDirection;
+                mailItem.CC = mailCopia;
+                mailItem.Send();
+                MessageBox.Show("Mensaje Envíado...", "Correo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                timer1.Enabled = false;
+                activarTemporizadorToolStripMenuItem.Enabled = true;
+            }
+            catch
+            {
+                MessageBox.Show("Mensaje Fellido...", "Correo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Settings.Default.enviado = false;
+                Settings.Default.Save();
+                timer1.Enabled = true;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            textBox1.Text += Settings.Default.enviado.ToString();
+            textBox2.Text = Settings.Default.fecha;
+            textBox3.Text = DateTime.Today.ToString("d");
+            if (Settings.Default.fecha != DateTime.Today.ToString("d")) 
+            {
+                Settings.Default.fecha = DateTime.Today.ToString("d");
+                Settings.Default.Save();
+                enviarCorreoToolStripMenuItem.Enabled = true;
+                Settings.Default.enviado = false;
+                Settings.Default.Save();
+            }
+            if ((DateTime.Now.ToString("HH:mm") == Settings.Default.hora.ToString()) && !Settings.Default.enviado )
+            {
+                //string mailDirection = @"Jhonny Paul Bedoya Guerrero <jhonny.bedoya@stracon.com>; Carmen Gavilan Vargas <carmen.gavilan@stracon.com>; Manuel Rodriguez Gutierrez <manuel.rodriguez@stracon.com>";
+                //string mailCopia = @"Gustavo Gomez Llanos <gustavo.gomez@stracon.com>; Ricardo Torres Inca <ricardo.torres@stracon.com>; Victor Antonio Calle Angeles <victor.calle@stracon.com>";
+
+                string mailDirection = @"Gregorio Jose Oduber Quevedo <gregorio.oduber@stracon.com>";
+                string mailCopia = @"Gregorio Jose Oduber Quevedo <gregorio.oduber@stracon.com>";
+
+                string mailSubject = "Inicio de Actividades del " + DateTime.Now.ToString("d");
+
+                string mainContent;
+                mainContent = "Estimados,";
+                mainContent += "<br>" + "<br>" + "Buenos Dias, Reportando Inicio de actividades Home Office el " + DateTime.Now.ToString("d");
+                mainContent += "<br>" + "<br>" + "Saludos,";
+                mainContent += "<br>" + "Gregorio Jose Oduber Quevedo";
+
+                SendEmailWithOutlook(mailDirection, mailCopia, mailSubject, mainContent);
+                Settings.Default.enviado = true;
+                Settings.Default.Save();
+                enviarCorreoToolStripMenuItem.Enabled = false;
+            }
+            //if (enviado == true)
+            //{
+            if (DateTime.Now.ToString("HH:mm") != Settings.Default.hora.ToString() && Settings.Default.enviado)
+                enviarCorreoToolStripMenuItem.Enabled = false;
+            //}
+            if (!Settings.Default.enviado && enviarCorreoToolStripMenuItem.Enabled == false)
+                enviarCorreoToolStripMenuItem.Enabled = true;
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            notifyIcon1.Icon = this.Icon;
+            notifyIcon1.ContextMenuStrip = contextMenuStrip1;
+            notifyIcon1.Text = Application.ProductName;
+            notifyIcon1.Visible = true;
+            this.Visible = false;
+        }
+
+        private void salirToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+                this.Visible = false;
+        }
+
+        private void enviarCorreoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Confirmar Envío de Correo Electronico", "Correo", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                string mailDirection = @"Jhonny Paul Bedoya Guerrero <jhonny.bedoya@stracon.com>; Carmen Gavilan Vargas <carmen.gavilan@stracon.com>; Manuel Rodriguez Gutierrez <manuel.rodriguez@stracon.com>";
+                string mailCopia = @"Gustavo Gomez Llanos <gustavo.gomez@stracon.com>; Ricardo Torres Inca <ricardo.torres@stracon.com>; Victor Antonio Calle Angeles <victor.calle@stracon.com>";
+                string mailSubject = "Inicio de Actividades del " + DateTime.Now.ToString("d");
+
+                string mainContent;
+                mainContent = "Estimados,";
+                mainContent += "<br>" + "<br>" + "Buenos Dias, Reportando Inicio de actividades Home Office el " + DateTime.Now.ToString("d");
+                mainContent += "<br>" + "<br>" + "Saludos,";
+                mainContent += "<br>" + "Gregorio Jose Oduber Quevedo";
+
+                SendEmailWithOutlook(mailDirection, mailCopia, mailSubject, mainContent);
+                Settings.Default.enviado = true;
+                enviarCorreoToolStripMenuItem.Enabled = false;
+            }
+            else
+                Settings.Default.enviado = false;
+            Settings.Default.Save();
+        }
+
+        private void TSTBHora_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                if (TSTBHora.ToString().Length > 0)
+                {
+                    Settings.Default.hora = TSTBHora.Text.ToString();
+                    Settings.Default.Save();
+                }
+                else
+                {
+                    MessageBox.Show("Especificar Hora para enviar correo:", "Correo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Settings.Default.hora = "07:00";
+                    Settings.Default.Save();
+                }
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (DateTime.Now > fechaActiva && !timer2.Enabled)
+            {
+                timer1.Enabled = true;
+                activarTemporizadorToolStripMenuItem.Enabled = false;
+            }
+        }
+
+        private void activarTemporizadorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            activarTemporizadorToolStripMenuItem.Enabled = false;
+        }
+    }
+}
